679ce60c7f7f9b5f5b16136e09f3dec583a91db6
JeremiahUnderwood <jeremiah.peter817@gmail.com>
2022-01-24 16:36:32 -0600

    Scripts Created

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,30 @@
+/**
+ * Created By Jeremiah Underwood (kinda)
+ * Date Created Jan 24 2022
+ * 
+ * Last edited by: NA
+ * Last edited: Jan 24 2022
+ * 
+ * Description: Creates Random Cubes in the scene
+  **/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
