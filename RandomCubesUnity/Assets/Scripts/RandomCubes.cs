caaa2b35baf7b639335bf7c4c36005523653321b
JeremiahUnderwood <jeremiah.peter817@gmail.com>
2022-01-26 16:41:52 -0600

    Proper Programming
    Made Random Cubes and color changes

--- RandomCubesUnity/Assets/Scripts/RandomCubes.cs
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -3,7 +3,7 @@
  * Date Created Jan 24 2022
  * 
  * Last edited by: NA
- * Last edited: Jan 24 2022
+ * Last edited: Jan 26 2022
  * 
  * Description: Creates Random Cubes in the scene
   **/
@@ -15,16 +15,48 @@ using UnityEngine;
 
 public class RandomCubes : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    //Variables
+    [SerializeField] private GameObject cubePrefab;               //serialize field to keep unneccecaraly public variables private, but still accessable through the editor
+    [SerializeField] private List<GameObject> gameObjectList;
+    [SerializeField] private float scalingFactor = 0.95f;
+    [SerializeField] private int numberOfCubes = 0;
     void Start()
     {
-        
+        gameObjectList = new List<GameObject>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate() //calls at a constant rate
     {
-        
+        numberOfCubes++;                                              //iterate number for cubes
+        GameObject gObj = Instantiate<GameObject>(cubePrefab);
+
+        gObj.name = "Cube" + numberOfCubes;                            //name cubes
+
+        gObj.transform.position = Random.insideUnitSphere;
+
+        Color randomColor = new Color(Random.value, Random.value, Random.value);
+        gObj.GetComponent<Renderer>().material.color = randomColor;
+
+        gameObjectList.Add(gObj);                                  //add cubes to list
+
+        List<GameObject> removeList = new List<GameObject>();      //newList
+
+        foreach (GameObject goTemp in gameObjectList)              //scales objects in list
+        {
+            float scale = goTemp.transform.localScale.x;
+            scale *= scalingFactor;
+            goTemp.transform.localScale = Vector3.one * scale;
+            if (scale <= 0.1f)            //mark smallest cubes for removal
+            {
+                removeList.Add(goTemp);
+            }
+        }
+
+        foreach (GameObject goTemp in removeList)            //remove and destroy smallest cubes cubes
+        {
+            gameObjectList.Remove(goTemp);
+            Destroy(goTemp);
+        }
     }
 }
